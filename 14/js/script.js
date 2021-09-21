ae14172095625ee1dcaaf6a7ad12e6ff6b1e3799
ART89-prog <72185684+ART89-prog@users.noreply.github.com>
2021-09-21 12:34:32 +0300

    first

---
+++ 14/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	
+
+	$('.accordion .item .head').click(function (e) {
+		e.preventDefault()
+
+		const $item = $(this).closest('.item'),
+			$accordion = $(this).closest('.accordion')
+
+		if ($item.hasClass('active')) {
+			$item.removeClass('active').find('.data').slideUp(300)
+		} else {
+			$accordion.find('.item').removeClass('active')
+			$accordion.find('.data').slideUp(300)
+
+			$item.addClass('active').find('.data').slideDown(300)
+		}
+	})
+
+});
